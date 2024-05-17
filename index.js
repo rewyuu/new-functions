1f280512dd47bfb7c4e0ed159185a6973869b9ec
rewyuu <119142506+rewyuu@users.noreply.github.com>
2024-05-17 17:56:41 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+
+menuToggleBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    menuToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header sticky & back to top
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+const filterButtons = document.querySelectorAll('.filter-btn');
+
+function handleButtonClick(event) {
+  console.log('Button clicked:', event.target.textContent);
+  filterButtons.forEach(button => button.classList.remove('active'));
+  event.target.classList.add('active');
+}
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', handleButtonClick);
+});
+
+
+
